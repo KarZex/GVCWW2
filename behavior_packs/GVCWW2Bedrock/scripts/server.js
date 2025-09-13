8975f64d8a586cc488ab4d03d655b3235866622b
しなもん <147789317+Densetsu4545@users.noreply.github.com>
2025-09-13 19:19:32 +0900

    www

--- behavior_packs/GVCWW2Bedrock/scripts/server.js
+++ behavior_packs/GVCWW2Bedrock/scripts/server.js
@@ -17,8 +17,9 @@ system.afterEvents.scriptEventReceive.subscribe(async (event) => {
         form.button("§3SMG Type§r\nPPSh41,TT33");
         form.button("§1Rifle Type§r\nMosin,TT33");
         form.button("§2LMG Type§r\nDP28,TT33");
-        form.button("§6Tank Type§r\nT34");
-        form.button("§eAircraft Type§r\nYak-9");
+        if( world.scoreboard.getObjective("cool").getScore(`SOV`) >= 6000 ){
+            form.button("§6Vehicle Option§r");
+        }
         form.show(user).then(result => {
             if(!result.canceled){
                 switch(result.selection){
@@ -50,17 +51,41 @@ system.afterEvents.scriptEventReceive.subscribe(async (event) => {
                         user.runCommand(`give @s zex:aidkit 2`);//Aid Kit
                         break;
                     case 3://Tank
-                        user.sendMessage("You selected §6Tank Type");
-                        user.runCommand(`ride @s summon_ride vehicle:t34`);
-                        user.runCommand(`give @s gun:mgg`);
-                        user.runCommand(`give @s gun:tank`);
-                        break;
-                    case 4:
-                        user.sendMessage("You selected §eAircraft Type");
-                        user.runCommand(`ride @s summon_ride vehicle:yak9`);
-                        user.runCommand(`give @s gun:mgg`);
-                        user.runCommand(`give @s gun:tank`);
-                        user.runCommand(`give @s gun:camera`);
+                        user.sendMessage("You selected §6Vehicle Option");
+                        const vehicle_form = new ActionFormData();
+                        vehicle_form.title("Vehicle Selection");
+                        vehicle_form.body("Select your vehicle type:");
+                        vehicle_form.button("§6Medium Tank Type§r\nT34");
+                        vehicle_form.button("§4Heavy Tank Type§r\nKV2");
+                        vehicle_form.button("§eFighter Type§r\nYak-9");
+                        vehicle_form.button("§bCAS Type§r\nIL2");
+                        vehicle_form.show(user).then(result => {
+                            if(!result.canceled){
+                                user.runCommand(`give @s gun:camera 1`);
+                                user.runCommand(`give @s gun:mgg 1`);
+                                user.runCommand(`give @s gun:tank 1`);
+                                user.runCommand(`give @s zex:mtype 1`); 
+                                world.scoreboard.getObjective("cool").addScore(`SOV`, -6000);
+                                switch(result.selection){
+                                    case 0:
+                                        user.sendMessage("You selected §6Medium Tank Type");
+                                        user.runCommand(`ride @s summon_ride vehicle:t34`);
+                                        break;
+                                    case 1:
+                                        user.sendMessage("You selected §4Heavy Tank Type");
+                                        user.runCommand(`ride @s summon_ride vehicle:kv2`);
+                                        break;
+                                    case 2:
+                                        user.sendMessage("You selected §eFighter Type");
+                                        user.runCommand(`ride @s summon_ride vehicle:yak9`);
+                                        break;
+                                    case 3:
+                                        user.sendMessage("You selected §bCAS Type");
+                                        user.runCommand(`ride @s summon_ride vehicle:il2`);
+                                        break;
+                                }
+                            }
+                        });
                         break;
                 }
             }
@@ -75,8 +100,7 @@ system.afterEvents.scriptEventReceive.subscribe(async (event) => {
         form.button("§3SMG Type§r\nMP40,Panzerfaust");
         //form.button("§2LMG Type§r\nMG42,P38");
         form.button("§1Ryfle Type§r\nKar98k, P38");
-        form.button("§6Tank Type§r\nPanzer IV");
-        form.button("§eAircraft Type§r\nMesserschmitt Bf 109");
+        form.button("§6Vehicle Option§r");
         form.show(user).then(result => {
             if(!result.canceled){
                 switch(result.selection){
@@ -110,17 +134,42 @@ system.afterEvents.scriptEventReceive.subscribe(async (event) => {
                         user.runCommand(`give @s gvcv5:fragment 4`);
                         break;
                     case 3://Tank
-                        user.sendMessage("You selected §6Tank Type");
-                        user.runCommand(`ride @s summon_ride vehicle:panzer`);
-                        user.runCommand(`give @s gun:mgg`);
-                        user.runCommand(`give @s gun:tank`);
-                        break;
-                    case 4:
-                        user.sendMessage("You selected §eAircraft Type");
-                        user.runCommand(`ride @s summon_ride vehicle:bf109`);
-                        user.runCommand(`give @s gun:mgg`);
-                        user.runCommand(`give @s gun:tank`);
-                        user.runCommand(`give @s gun:camera`);
+                        user.sendMessage("You selected §6Vehicle Option");
+                        const vehicle_form = new ActionFormData();
+                        vehicle_form.title("Vehicle Selection");
+                        vehicle_form.body("Select your vehicle type:");
+                        vehicle_form.button("§4Medium Tank Type§r\nPanzer IV");
+                        vehicle_form.button("§3Heavy Tank Type§r\nTiger I");
+                        vehicle_form.button("§eFighter Type§r\nbf109");
+                        vehicle_form.button("§bCAS Type§r\nJu87");
+                        vehicle_form.show(user).then(result => {
+                            if(!result.canceled){
+                                user.runCommand(`give @s gun:camera 1`);
+                                user.runCommand(`give @s gun:mgg 1`);
+                                user.runCommand(`give @s gun:tank 1`);
+                                user.runCommand(`give @s zex:mtype 1`); 
+                                world.scoreboard.getObjective("cool").addScore(`GER`, -6000);
+                                switch(result.selection){
+                                    case 0:
+                                        user.sendMessage("You selected §4Medium Tank Type");
+                                        user.runCommand(`ride @s summon_ride vehicle:panzer`);
+                                        break;
+                                    case 1:
+                                        user.sendMessage("You selected §3Heavy Tank Type");
+                                        user.runCommand(`ride @s summon_ride vehicle:tiger`);
+                                        break;
+                                    case 2:
+                                        user.sendMessage("You selected §eFighter Type");
+                                        user.runCommand(`ride @s summon_ride vehicle:bf109`);
+                                        break;
+                                    case 3:
+                                        user.sendMessage("You selected §bCAS Type");
+                                        user.runCommand(`ride @s summon_ride vehicle:ju87`);
+                                        break;
+                                }
+
+                            }
+                        });
                         break;
                 }
             }
