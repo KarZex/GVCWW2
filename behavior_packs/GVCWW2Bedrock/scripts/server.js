ea43ed0ea43662f907d62fb2fe6ed298d66fcf06
Kar Zex <152583031+KarZex@users.noreply.github.com>
2025-09-13 18:10:08 +0900

    q

--- behavior_packs/GVCWW2Bedrock/scripts/server.js
+++ behavior_packs/GVCWW2Bedrock/scripts/server.js
@@ -10,12 +10,46 @@ system.afterEvents.scriptEventReceive.subscribe(async (event) => {
          
     }
     if( event.id === "gvcww2:startfly" ){
-        const player = event.sourceEntity;
-        const m = event.message;
-        player.teleport({x:0,y:120,z:0});
-        await system.waitTicks(5)
-        player.runCommand(`ride @s summon_ride vehicle:${m}`);
-        player.addTag(`flying`);
-         
+        const form = new ActionFormData();
+        form.title("GERチーム兵科選択");
+        form.body("兵科を選択してください");
+        form.button("アサルト");
+        form.button("SMG");
+        form.button("LMG");
+        form.button("スナイパー");
+        form.button("戦車");
+        form.button("飛行機");
+        form.show(user).then(result => {
+            if(!result.canceled){
+                switch(result.selection){
+                    case 0:
+                        user.sendMessage("アサルトを選択しました");
+                        user.runCommand(`give @s gun:stg44`);
+                        user.runCommand(`give @s gun:p90`);
+                        user.runCommand(`give @s zex:762m 180`);
+                        break;
+                    case 1:
+                        user.sendMessage("SMGを選択しました");
+                        // SMG用の処理
+                        break;
+                    case 2:
+                        user.sendMessage("LMGを選択しました");
+                        // LMG用の処理
+                        break;
+                    case 3:
+                        user.sendMessage("スナイパーを選択しました");
+                        // スナイパー用の処理
+                        break;
+                    case 4:
+                        user.sendMessage("戦車を選択しました");
+                        // 戦車用の処理
+                        break;
+                    case 5:
+                        user.sendMessage("飛行機を選択しました");
+                        // 飛行機用の処理
+                        break;
+                }
+            }
+        });
     }
 });
