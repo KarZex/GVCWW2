e4c51190a2a02f3774ffe443907c8e13173e7d80
しなもん <147789317+Densetsu4545@users.noreply.github.com>
2025-09-13 18:25:38 +0900

    q

--- behavior_packs/GVCWW2Bedrock/scripts/server.js
+++ behavior_packs/GVCWW2Bedrock/scripts/server.js
@@ -9,44 +9,118 @@ system.afterEvents.scriptEventReceive.subscribe(async (event) => {
         }
          
     }
-    if( event.id === "gvcww2:startfly" ){
+    if( event.id === "gvcww2:selectWeaponSOV" ){
+        const user = event.sourceEntity;
         const form = new ActionFormData();
-        form.title("GERチーム兵科選択");
-        form.body("兵科を選択してください");
-        form.button("アサルト");
-        form.button("SMG");
-        form.button("LMG");
-        form.button("スナイパー");
-        form.button("戦車");
-        form.button("飛行機");
+        form.title("Soviet Team Weapon Selection");
+        form.body("Select your weapon type:");
+        form.button("§3SMG Type§r\nPPSh41,TT33");
+        form.button("§1Rifle Type§r\nMosin,TT33");
+        form.button("§2LMG Type§r\nDP28,TT33");
+        form.button("§6Tank Type§r\nT34");
+        form.button("§eAircraft Type§r\nYak-9");
         form.show(user).then(result => {
             if(!result.canceled){
                 switch(result.selection){
                     case 0:
-                        user.sendMessage("アサルトを選択しました");
-                        user.runCommand(`give @s gun:stg44`);
-                        user.runCommand(`give @s gun:p90`);
-                        user.runCommand(`give @s zex:762m 180`);
+                        user.sendMessage("You selected §3SMG Type");
+                        user.runCommand(`give @s minecraft:iron_sword`);//Melee Weapon
+                        user.runCommand(`give @s gun:ppsh`);
+                        user.runCommand(`give @s gun:tt33`);
+                        user.runCommand(`give @s zex:mm9 600`);
+                        user.runCommand(`give @s gvcv5:fragment 4`);//Grenade
+                        user.runCommand(`give @s zex:aidkit 2`);//Aid Kit
                         break;
                     case 1:
-                        user.sendMessage("SMGを選択しました");
-                        // SMG用の処理
+                        user.sendMessage("You selected §1Rifle Type");
+                        user.runCommand(`give @s minecraft:iron_sword`);
+                        user.runCommand(`give @s gun:mosin`);
+                        user.runCommand(`give @s gun:tt33`);
+                        user.runCommand(`give @s zex:762m 120`);
+                        user.runCommand(`give @s gvcv5:fragment 4`);//Grenade
+                        user.runCommand(`give @s zex:aidkit 2`);//Aid Kitq
                         break;
                     case 2:
-                        user.sendMessage("LMGを選択しました");
-                        // LMG用の処理
+                        user.sendMessage("You selected §2LMG Type");
+                        user.runCommand(`give @s minecraft:iron_sword`);
+                        user.runCommand(`give @s gun:dp28`);
+                        user.runCommand(`give @s gun:tt33`);
+                        user.runCommand(`give @s zex:762m 240`);
+                        user.runCommand(`give @s gvcv5:fragment 4`);
+                        user.runCommand(`give @s zex:aidkit 2`);//Aid Kit
                         break;
-                    case 3:
-                        user.sendMessage("スナイパーを選択しました");
-                        // スナイパー用の処理
+                    case 3://Tank
+                        user.sendMessage("You selected §6Tank Type");
+                        user.runCommand(`ride @s summon_ride vehicle:t34`);
+                        user.runCommand(`give @s gun:mgg`);
+                        user.runCommand(`give @s gun:tank`);
                         break;
                     case 4:
-                        user.sendMessage("戦車を選択しました");
-                        // 戦車用の処理
+                        user.sendMessage("You selected §eAircraft Type");
+                        user.runCommand(`ride @s summon_ride vehicle:yak9`);
+                        user.runCommand(`give @s gun:mgg`);
+                        user.runCommand(`give @s gun:tank`);
+                        user.runCommand(`give @s gun:camera`);
                         break;
-                    case 5:
-                        user.sendMessage("飛行機を選択しました");
-                        // 飛行機用の処理
+                }
+            }
+        });
+    }
+    if( event.id === "gvcww2:selectWeaponGER" ){
+        const user = event.sourceEntity;
+        const form = new ActionFormData();
+        form.title("German Team Weapon Selection");
+        form.body("Select your weapon type:");
+        form.button("§4Assault Type§r\nSTG44,Panzerfaust");
+        form.button("§3SMG Type§r\nMP40,Panzerfaust");
+        //form.button("§2LMG Type§r\nMG42,P38");
+        form.button("§1Ryfle Type§r\nKar98k, P38");
+        form.button("§6Tank Type§r\nPanzer IV");
+        form.button("§eAircraft Type§r\nMesserschmitt Bf 109");
+        form.show(user).then(result => {
+            if(!result.canceled){
+                switch(result.selection){
+                    case 0:
+                        user.sendMessage("You selected §4Assault Type");
+                        user.runCommand(`give @s minecraft:iron_sword`);//Melee Weapon
+                        user.runCommand(`give @s gun:stg44`);//STG44
+                        user.runCommand(`give @s gun:p90`);//Panzerfaust
+                        user.runCommand(`give @s zex:762m 240`);
+                        user.runCommand(`give @s zex:rocketm 4`);
+                        user.runCommand(`give @s gvcv5:fragment 4`);//Grenade
+                        user.runCommand(`give @s zex:aidkit 2`);//Aid Kit
+                        break;
+                    case 1:
+                        user.sendMessage("You selected §3SMG Type");
+                        user.runCommand(`give @s minecraft:iron_sword`);//Melee Weapon
+                        user.runCommand(`give @s gun:mp40`);
+                        user.runCommand(`give @s gun:p90`);
+                        user.runCommand(`give @s zex:mm9 320`);
+                        user.runCommand(`give @s zex:rocketm 4`);
+                        user.runCommand(`give @s gvcv5:fragment 4`);//Grenade
+                        user.runCommand(`give @s zex:aidkit 2`);//Aid Kit
+                        break;
+                    case 2: //Sniper
+                        user.sendMessage("You selected §1Sniper Type");
+                        user.runCommand(`give @s minecraft:iron_sword`);//Melee Weapon
+                        user.runCommand(`give @s gun:k98`);
+                        user.runCommand(`give @s gun:p38`);
+                        user.runCommand(`give @s zex:762m 64`);
+                        user.runCommand(`give @s zex:rocketm 4`);
+                        user.runCommand(`give @s gvcv5:fragment 4`);
+                        break;
+                    case 3://Tank
+                        user.sendMessage("You selected §6Tank Type");
+                        user.runCommand(`ride @s summon_ride vehicle:panzer`);
+                        user.runCommand(`give @s gun:mgg`);
+                        user.runCommand(`give @s gun:tank`);
+                        break;
+                    case 4:
+                        user.sendMessage("You selected §eAircraft Type");
+                        user.runCommand(`ride @s summon_ride vehicle:bf109`);
+                        user.runCommand(`give @s gun:mgg`);
+                        user.runCommand(`give @s gun:tank`);
+                        user.runCommand(`give @s gun:camera`);
                         break;
                 }
             }
